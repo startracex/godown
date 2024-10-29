27707116aa7c7f9f283ae3ffe30fce116f2da888
STARTRACEX <startraceebr@foxmail.com>
2024-10-29 10:03:22 +0800

    update loading behavior

--- packages/f7-icon/element.ts
+++ packages/f7-icon/element.ts
@@ -1,17 +1,24 @@
-import { alias } from "@godown/element/decorators/alias.js";
 import GodownElement from "@godown/element/element.js";
 import { css } from "@lit/reactive-element/css-tag.js";
 import { property } from "@lit/reactive-element/decorators/property.js";
 import { state } from "@lit/reactive-element/decorators/state.js";
 
+const importSpec = (specifier: string | URL, base: string | URL) =>
+  (typeof specifier === "string" && import.meta.resolve)
+    ? import.meta.resolve(specifier)
+    : new URL(specifier, base || undefined) + "";
+
 class IconElement extends GodownElement {
   static styles = css`:host{width:1em;height:1em;display:inline-block}svg{height:100%;width:100%;display:block}`;
 
   @state()
-  toURL: (id: string) => string | URL = (id) => `./icons/${id}.js`;
+  toURL: (this: IconElement, name: string) => string | URL = (id) => `./icons/${id}.js`;
 
   @property()
   loading: "lazy" | "eager" = "lazy";
+
+  @property()
+  base = import.meta.url;
 
   @property()
   name: string;
@@ -19,11 +26,11 @@ class IconElement extends GodownElement {
   @state()
   module: any;
 
-  protected allowLoad: boolean;
+  protected allowLoad?: boolean;
 
-  load(id: string) {
+  load() {
     if (this.name && this.allowLoad) {
-      import(this.toURL(id) + "").then((i) => this.module = i);
+      import(importSpec(this.toURL(this.name), this.base)).then((i) => this.module = i);
     }
   }
 
@@ -35,13 +42,13 @@ class IconElement extends GodownElement {
     super.connectedCallback();
     if (this.loading === "eager") {
       this.allowLoad = true;
-      this.load(this.name);
+      this.load();
     } else {
       const handleIntersection: IntersectionObserverCallback = (entries, observer) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
             this.allowLoad = true;
-            this.load(this.name);
+            this.load();
             observer.unobserve(this);
           }
         });
@@ -54,7 +61,7 @@ class IconElement extends GodownElement {
   attributeChangedCallback(name: string, old: string, value: string | null): void {
     super.attributeChangedCallback(name, old, value);
     if (name === "name" && value) {
-      this.load(value);
+      this.load();
     }
   }
 }
