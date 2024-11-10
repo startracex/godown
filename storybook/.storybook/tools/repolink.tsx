36aeb5d689c26fdc534c72ea05ac03a3f3df4500
STARTRACEX <startraceebr@foxmail.com>
2024-11-10 13:58:09 +0800

    fix bookstory repolink

--- storybook/.storybook/tools/repolink.tsx
+++ storybook/.storybook/tools/repolink.tsx
@@ -4,11 +4,12 @@ import { GithubIcon } from "@storybook/icons";
 import React, { memo } from "react";
 
 export const repoLink = memo(() => {
-  const { storyId } = useStorybookState();
+  const { storyId, index } = useStorybookState();
   let href = "https://github.com/startracex/godown/blob/next";
-  if (storyId) {
-    const [category, name] = storyId.split("-");
-    href += `/storybook/category/${category}/${name}.stories.ts`;
+
+  const path = (index?.[storyId] as any)?.importPath?.slice(1);
+  if (path) {
+    href += "/storybook" + path;
   }
 
   return (
