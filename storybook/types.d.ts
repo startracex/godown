b1214f9188a6d29ce15a66f514eef247cfd23808
STARTRACEX <startraceebr@foxmail.com>
2024-11-24 08:36:49 +0800

    move types.ts to types.d.ts

--- storybook/types.d.ts
+++ storybook/types.d.ts
@@ -2,3 +2,9 @@ declare module "*.mdx" {
   let MDXComponent: (props: any) => JSX.Element;
   export default MDXComponent;
 }
+
+import { type Meta } from "@storybook/web-components";
+
+export type RendererMeta<T extends (...args: any) => any> = Meta<Parameters<T>[0]>;
+
+export type * as Godown from "godown/src/index.ts";
