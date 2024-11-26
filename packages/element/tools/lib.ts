ad06dd7440d035b6d4111ae3f90c8b76a57414b4
STARTRACEX <startraceebr@foxmail.com>
2024-11-26 10:30:17 +0800

    add omit and pick utility functions to lib.ts

--- packages/element/tools/lib.ts
+++ packages/element/tools/lib.ts
@@ -53,3 +53,27 @@ export const trimLeft = (s: string, spec = " ") => {
 export const trim = (s: string, spec?: string) => {
   return trimRight(trimLeft(s, spec), spec);
 };
+
+/**
+ * Construct a object with the properties of obj except for those in keys.
+ */
+export const omit = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> => {
+  return Object.keys(obj).reduce((acc, key) => {
+    if (!keys.includes(key as K)) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {} as Omit<T, K>);
+};
+
+/**
+ * From obj, pick a set of properties whose keys are in keys.
+ */
+export const pick = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> => {
+  return keys.reduce((acc, key) => {
+    if (key in obj) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {} as Pick<T, K>);
+};
