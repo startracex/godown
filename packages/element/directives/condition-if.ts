559710140148794c1527ca4d16f07a16bfcfd937
STARTRACEX <startraceebr@foxmail.com>
2024-12-06 18:52:51 +0800

    remove unused directives

--- packages/element/directives/condition-if.ts
+++
@@ -1,8 +0,0 @@
-import { nothing } from "lit";
-
-interface RenderIf {
-  <A, B>(value: unknown, a: A, b: B): A | B;
-  <A>(value: unknown, a: A): A;
-}
-
-export const conditionIf: RenderIf = <A, B>(value: unknown, a: A, b?: B) => (value && a) || b || nothing;
--- packages/element/directives/index.ts
+++ packages/element/directives/index.ts
@@ -1,6 +1,4 @@
 export * from "./attr.js";
 export * from "./class-list.js";
-export * from "./condition-if.js";
 export * from "./html-slot.js";
 export * from "./html-style.js";
-export * from "./through.js";
