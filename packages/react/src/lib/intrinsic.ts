96e29603a9944c492a0db107a0cfbda076e1a170
STARTRACEX <startraceebr@foxmail.com>
2024-12-06 23:02:18 +0800

    enhance intrinsic type declaration

--- packages/react/src/lib/intrinsic.ts
+++ packages/react/src/lib/intrinsic.ts
@@ -1,4 +1,8 @@
 import type * as React from "react";
 
-export type IntrinsicElement<T extends HTMLElement> = React.DetailedHTMLProps<React.HTMLAttributes<T> & Partial<T>, T>;
+export type IntrinsicElement<T extends HTMLElement> = React.DetailedHTMLProps<
+  & React.HTMLAttributes<T>
+  & Partial<Omit<T, keyof HTMLElement>>,
+  T
+>;
 export type IntrinsicTag<T extends keyof HTMLElementTagNameMap> = IntrinsicElement<HTMLElementTagNameMap[T]>;
