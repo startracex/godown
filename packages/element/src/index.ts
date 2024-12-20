8b35bcf3465d997b70e1d7aa5ee0455c33e26712
STARTRACEX <startraceebr@foxmail.com>
2024-12-21 01:17:58 +0800

    add exports entry

---
+++ packages/element/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./config.js";
+export * from "./decorators/index.js";
+export * from "./directives/index.js";
+export { default } from "./element.js";
+export * from "./element.js";
+export * from "./tools/index.js";
