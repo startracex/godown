0b06e791c9419dabae486a5777b0adb2f14d0c51
STARTRACEX <startraceebr@foxmail.com>
2024-12-20 18:29:39 +0800

    simplify the direct return arrow functions

--- packages/element/tools/lib.ts
+++ packages/element/tools/lib.ts
@@ -47,30 +47,26 @@ export const trimLeft = (s: string, spec = " "): string => {
   return s.slice(start);
 };
 
-export const trim = (s: string, spec?: string): string => {
-  return trimRight(trimLeft(s, spec), spec);
-};
+export const trim = (s: string, spec?: string): string => trimRight(trimLeft(s, spec), spec);
 
 /**
  * Construct a object with the properties of obj except for those in keys.
  */
-export const omit = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> => {
-  return Object.keys(obj).reduce((acc, key) => {
+export const omit = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> =>
+  Object.keys(obj).reduce((acc, key) => {
     if (!keys.includes(key as K)) {
       acc[key] = obj[key];
     }
     return acc;
   }, {} as Omit<T, K>);
-};
 
 /**
  * From obj, pick a set of properties whose keys are in keys.
  */
-export const pick = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> => {
-  return keys.reduce((acc, key) => {
+export const pick = <T extends object, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> =>
+  keys.reduce((acc, key) => {
     if (key in obj) {
       acc[key] = obj[key];
     }
     return acc;
   }, {} as Pick<T, K>);
-};
