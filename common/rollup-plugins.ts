8c55e969936b29adf96c6cc3c0b88cb072590001
STARTRACEX <startraceebr@foxmail.com>
2024-12-22 03:57:13 +0800

    optimize exports for common

--- common/rollup-plugins.ts
+++ common/rollup-plugins.ts
@@ -1,12 +1,14 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import _terser, { type Options as TerserOptions } from "@rollup/plugin-terser";
-import { cjsShim } from "rollup-plugin-cjs-shim";
 import _minifyLiterals from "rollup-plugin-minify-html-literals";
-import templateReplace from "rollup-plugin-template-replace";
-import ts2 from "rollup-plugin-typescript2";
-const minifyLiterals = (_minifyLiterals as any).default as typeof _minifyLiterals;
 
-const terser = (options: Omit<TerserOptions, "ecma"> & Record<string, any> = {}) => _terser(options);
+export const minifyLiterals = (_minifyLiterals as any).default as typeof _minifyLiterals;
+export const terser = (options: Omit<TerserOptions, "ecma"> & Record<string, any> = {}) => _terser(options);
 
-export { cjsShim, commonjs, minifyLiterals, nodeResolve, templateReplace, terser, ts2 };
+export { default as commonjs } from "@rollup/plugin-commonjs";
+export { nodeResolve } from "@rollup/plugin-node-resolve";
+export { default as ts2 } from "rollup-plugin-typescript2";
+
+// workspace scope
+export { cjsShim } from "rollup-plugin-cjs-shim";
+export { pruneImports } from "rollup-plugin-prune-imports";
+export { default as templateReplace } from "rollup-plugin-template-replace";
