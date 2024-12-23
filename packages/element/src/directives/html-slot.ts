d7d2605ca52c6f9a210cf016c9cd8bf2a24fdf41
STARTRACEX <startraceebr@foxmail.com>
2024-12-23 16:37:56 +0800

    add slotChange parameter for htmlSlot

--- packages/element/src/directives/html-slot.ts
+++ packages/element/src/directives/html-slot.ts
@@ -1,10 +1,14 @@
 import { html, nothing, type TemplateResult } from "lit";
 
+import type { EventListenerFunc } from "../tools/events.js";
+
 /**
  * Slot element directive.
  *
  * @param name Slot name, if undefined, no name attribute will be added.
+ * @param slotChange Slot change event listener.
  */
-export const htmlSlot = (name?: string): TemplateResult<1> => html`<slot name="${name || nothing}"></slot>`;
+export const htmlSlot = (name?: string, slotChange?: EventListenerFunc): TemplateResult<1> =>
+  html`<slot name="${name || nothing}" @slotchange=${slotChange || null}></slot>`;
 
 export default htmlSlot;
