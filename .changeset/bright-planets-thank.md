4869824a1e0d0a45ea581a564a499cab12b28230
STARTRACEX <startraceebr@foxmail.com>
2025-01-31 03:07:23 +0800

    remove deprecated classList directive

---
+++ .changeset/bright-planets-thank.md
@@ -0,0 +1,5 @@
+---
+"@godown/element": minor
+---
+
+remove deprecated classList directive
--- packages/element/src/directives/index.ts
+++ packages/element/src/directives/index.ts
@@ -1,5 +1,4 @@
 export * from "./attr.js";
-export { classList } from "./class-list.js";
 export * from "./html-slot.js";
 export * from "./html-style.js";
 export * from "./icon.js";
