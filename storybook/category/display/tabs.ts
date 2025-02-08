5c3785da074da9743b33ae017db0e421516a67fd
STARTRACEX <startraceebr@foxmail.com>
2025-02-09 03:16:37 +0800

    add slot content to tabs story

--- storybook/category/display/tabs.ts
+++ storybook/category/display/tabs.ts
@@ -4,6 +4,20 @@ import { html } from "lit";
 
 export default (args: Tabs) => {
   return html`
-    <godown-tabs ${attr(args)}></godown-tabs>
+<godown-tabs ${attr(args)}>
+${
+    args.tabs.map(
+      (tab) =>
+        html`
+  <div
+    slot="${tab}"
+    style="padding: 0 .5em;white-space: nowrap;"
+  >
+    Slot ${tab}
+  </div>`,
+    )
+  }
+
+</godown-tabs>
   `;
 };
