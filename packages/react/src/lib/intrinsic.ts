61ee03544567204aea9adadd03c57ad1d24bc9d9
STARTRACEX <startraceebr@foxmail.com>
2025-02-12 18:24:48 +0800

    optimize types

--- packages/react/src/lib/intrinsic.ts
+++ packages/react/src/lib/intrinsic.ts
@@ -1,10 +1,12 @@
 import type * as React from "react";
 
-export type IntrinsicElement<T extends HTMLElement> = React.DetailedHTMLProps<
-  & React.HTMLAttributes<T>
-  & Partial<Omit<T, keyof HTMLElement>>,
+import type { WebComponentProps } from "@lit/react";
+
+export type _IntrinsicElement<T extends HTMLElement> = React.DetailedHTMLProps<
+  React.HTMLAttributes<T> & Partial<Omit<T, keyof HTMLElement>>,
   T
 >;
+export type IntrinsicElement<T extends HTMLElement> = WebComponentProps<T>;
 export type IntrinsicTag<T extends keyof HTMLElementTagNameMap> = IntrinsicElement<HTMLElementTagNameMap[T]>;
 
 /**
