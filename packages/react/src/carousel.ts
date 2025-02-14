ed299677cd85e64234bf433d12fb858ed0f0aa4b
STARTRACEX <startraceebr@foxmail.com>
2025-02-14 16:34:36 +0800

    add missing events declaration

--- packages/react/src/carousel.ts
+++ packages/react/src/carousel.ts
@@ -1,10 +1,17 @@
 "use client";
 import Carousel from "godown/carousel.js";
 
-import { createReact } from "./lib/create.js";
+import { createReact, eventMap, type TargetedCustomEvent } from "./lib/create.js";
 import type { IntrinsicElement } from "./lib/intrinsic.js";
 
-export default createReact(Carousel);
+export default createReact(
+  Carousel,
+  eventMap<{
+    onChange: TargetedCustomEvent<number, Carousel>;
+  }>({
+    onChange: "change",
+  }),
+);
 
 export * from "godown/carousel.js";
 
