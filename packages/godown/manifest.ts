98bf63c683639dc41146dabb3209bc3e4e2746d1
STARTRACEX <startraceebr@foxmail.com>
2025-02-28 02:28:46 +0800

    add file size logging for JSON outputs

--- packages/godown/manifest.ts
+++ packages/godown/manifest.ts
@@ -3,6 +3,7 @@ import { globSync } from "glob";
 import { analyze } from "@godown/common/cem";
 import { fixModule, jb, moduleDeclarationDefine, vs } from "@godown/common/cem-plugins";
 import { minJSON } from "@godown/common/min-json";
+import { statSync } from "node:fs";
 
 function toJSPath(path: string) {
   return path
@@ -22,4 +23,18 @@ await analyze({
   cwd: import.meta.dirname,
 });
 
-["custom-elements.json", "web-types.json", "vscode.css-custom-data.json", "vscode.html-custom-data.json"].map(minJSON);
+const jsonFiles = [
+  "custom-elements.json",
+  "web-types.json",
+  "vscode.css-custom-data.json",
+  "vscode.html-custom-data.json",
+];
+
+jsonFiles.forEach(minJSON);
+
+jsonFiles.forEach((file) => {
+  console
+    .info(`${file.padEnd(Math.max(...jsonFiles.map((item) => item.length)))} (${
+      (statSync(file).size / 1024).toFixed(1) + " KiB"
+    })`);
+});
