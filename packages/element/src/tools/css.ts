336479c7c9df0d97c8319a43ef8f349ce4265e0f
STARTRACEX <startraceebr@foxmail.com>
2025-03-03 16:27:59 +0800

    optimize CSSResult handling

--- packages/element/src/tools/css.ts
+++ packages/element/src/tools/css.ts
@@ -1,14 +1,18 @@
 import type { CSSResult } from "lit";
 
-import { type Entry, isNullable, isObject, toEntries } from "./lib.js";
+import { type Entry, isNullable, isObject, isString, toEntries } from "./lib.js";
 
 type LikeString = string | { toString(): string };
+
+const isCSSResult = (value: unknown): value is CSSResult => {
+  return isObject(value) && "_$cssResult$" in value;
+};
 
 export const joinRules = (rules: Entry<string, string | Entry<LikeString>>): string => {
   let result = "";
   for (const [key, value] of toEntries(rules)) {
     if (value) {
-      const properties = isObject(value) && !("_$cssResult$" in value) ? joinDeclarations(value) : value;
+      const properties = isObject(value) && !isCSSResult(value) ? joinDeclarations(value) : value;
       if (properties) {
         result += key ? `${key}{${properties}}` : properties;
       }
@@ -30,10 +34,13 @@ export const joinDeclarations = (props: Entry<LikeString>): string => {
 export const toVar = (a: LikeString, b?: LikeString): string => (a ? `var(${a}${b ? `,${b}` : ""})` : "");
 
 export const toStyleSheet = (style: string | CSSStyleSheet | CSSResult): CSSStyleSheet => {
-  if (!(style instanceof CSSStyleSheet)) {
+  if (isString(style)) {
     const sheet = new CSSStyleSheet();
-    sheet.replaceSync(String(style));
+    sheet.replaceSync(style);
     return sheet;
+  }
+  if (isCSSResult(style)) {
+    return style.styleSheet;
   }
   return style;
 };
