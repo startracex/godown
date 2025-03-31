d1e16d49a2c9b596aa3ad90094b4f5956074d0c7
STARTRACEX <startraceebr@foxmail.com>
2025-04-01 00:15:56 +0800

    add is functions

---
+++ .changeset/wicked-seas-work.md
@@ -0,0 +1,5 @@
+---
+"sharekit": minor
+---
+
+add is functions
--- packages/sharekit/src/is.ts
+++ packages/sharekit/src/is.ts
@@ -36,4 +36,30 @@ export const isTemplateStringArray = (value: unknown): value is TemplateStringsA
   isArray(value) && isArray((value as any).raw);
 
 export const isConstructor = <T = any>(value: unknown): value is Constructor<T> =>
-  typeof value === "function" && value.prototype?.constructor === value;
+  isFunction(value) && value.prototype.constructor === value;
+
+const asyncFnCons = (async () => {}).constructor;
+
+export const isAsyncFunction = (value: unknown): value is (...args: any[]) => Promise<any> =>
+  isFunction(value) && value.constructor === asyncFnCons;
+
+const generatorFnCons = function* () {}.constructor;
+
+export const isGeneratorFunction = (value: unknown): value is (...args: any[]) => Generator<any, any, any> =>
+  isFunction(value) && value.constructor === generatorFnCons;
+
+const asyncGeneratorFnCons = (async function* () {}).constructor;
+
+export const isAsyncGeneratorFunction = (value: unknown): value is (...args: any[]) => AsyncGenerator<any, any, any> =>
+  isFunction(value) && value.constructor === asyncGeneratorFnCons;
+
+export const isIterable = <T = any>(value: unknown): value is Iterable<T> =>
+  isObject(value) && isFunction((value as any)[Symbol.iterator]);
+
+export const isAsyncIterable = <T = any>(value: unknown): value is AsyncIterable<T> =>
+  isObject(value) && isFunction((value as any)[Symbol.asyncIterator]);
+
+export const isAwaitable = <T = any>(value: unknown): value is Promise<T> => !isFunction((value as any)?.then);
+
+export const isThenable = <T = any>(value: unknown): value is Awaited<T> =>
+  (isObject(value) || isFunction(value)) && !isAwaitable(value);
