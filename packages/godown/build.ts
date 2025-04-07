e52dff6a76f3e22e88d6e0c05c336a23d1856d44
STARTRACEX <startraceebr@foxmail.com>
2025-04-07 18:08:45 +0800

    fix sourcemap not match

--- packages/godown/build.ts
+++ packages/godown/build.ts
@@ -5,7 +5,7 @@ import { autoprefixer, cssnano } from "@godown/common/third-party/postcss";
 import { terser, ts } from "@godown/common/third-party/rollup";
 import { minifyHtmlParts, templateReplace } from "@godown/common/workspace-scoped/rollup";
 import { build, commonInput, commonOutput } from "@godown/common/rollup-creator";
-
+import { basename, join } from "node:path";
 const dir = ".";
 
 const input = globSync("src/**/*.ts");
@@ -25,15 +25,25 @@ await build(
       minifyHtmlParts(),
       ts({
         tsconfig: "./tsconfig.prod.json",
+        compilerOptions: {
+          sourceRoot: "__source__",
+        },
       }),
       terser({
-        ecma: 2021,
         keep_classnames: true,
       }),
     ],
   },
   {
     ...commonOutput,
+    sourcemapPathTransform(s) {
+      if (!s.includes("__source__")) {
+        return s;
+      }
+      const [dir, wrongPath] = s.split("__source__");
+      const base = basename(wrongPath);
+      return join(dir, base);
+    },
     dir,
   },
 ).then(() => {
