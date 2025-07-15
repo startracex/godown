ffad9b0b4027a7f928b4d2e2f9bfbd093faf109f
Shiro Wang <startraceebr@foxmail.com>
2025-07-15 12:24:33 +0800

    remove legacy packages

--- legacy/f7-icon/f7-icon.ts
+++
@@ -1,9 +0,0 @@
-import { IconElement } from "./element.js";
-IconElement.define("f7-icon");
-export { IconElement as default, IconElement as F7Icon };
-
-declare global {
-  interface HTMLElementTagNameMap {
-    "f7-icon": IconElement;
-  }
-}
